f7c5bb97475d7fb0884dd63b09f70fb4dbb1968f
Luke <lukeledvina19@gmail.com>
2022-10-23 00:37:01 -0400

    Sup

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // Work on this project, as well as
 // work on Think Like A Programmer
+// Don't stress yourself out about it
+// If you just sit down and start
+// It is quite fun in fact
+// :)))) grind this shit out :)
